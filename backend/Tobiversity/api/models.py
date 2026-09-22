e123adca3a462b95145210309018dddeec400769
tobi-dnme <tobimanuelolu@gmail.com>
2026-09-22 14:31:15 +0100

    Added a 'date_added' field to the Course model and registered it in admin.py

--- backend/Tobiversity/api/models.py
+++ backend/Tobiversity/api/models.py
@@ -3,5 +3,7 @@ from django.db import models
 class Course(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
+    date_added = models.DateField(auto_now_add=True)
+    
     def __str__(self):
         return self.title
