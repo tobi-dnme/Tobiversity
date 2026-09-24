8ff08fe7de26a39f547d3268274fb9fa1bb9241f
tobi-dnme <tobimanuelolu@gmail.com>
2026-09-24 01:07:01 +0100

    Added PATCH and DELETE methods to CourseView

--- backend/Tobiversity/api/views.py
+++ backend/Tobiversity/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from rest_framework import status
 from .models import Course
 from .serializers import CourseSerializer
 
@@ -13,7 +13,54 @@ class CourseView(APIView):
 
     def post(self, request):
         serializer = CourseSerializer(data=request.data)
+
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk=None):
+        if not pk:
+            return Response(
+                {"error": "I'm gonna need your Course ID"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        try:
+            course = Course.objects.get(id=pk)
+        except Course.DoesNotExist:
+            return Response(
+                {"error": "Don't think we have that course here"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        serializer = CourseSerializer(course, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def delete(self, request, pk=None):
+        if not pk:
+            return Response(
+                {"error": "I'm gonna need your Course ID"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        try:
+            course = Course.objects.get(id=pk)
+        except Course.DoesNotExist:
+            return Response(
+                {"error": "Don't think we have that course here"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        course.delete()
+        return Response(
+            {"message": "Course deleted"},
+            status=status.HTTP_204_NO_CONTENT
+        )
